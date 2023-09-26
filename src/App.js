ad881787b740ffd3514a5278fa2d2dc7e2a02aa0
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-09-26 09:19:47 +0300

    position the card and adding basic styles

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Hi
+    <div className='wraper grid-area'>
+      <div className='card grid-area'>
+        <img src='./images/icon-star.svg'/>
+        <h1>How did we do?</h1>
+        <p>Please let us Know how we did with your supprort request. All feedback is appreciated to help us improve our offering!</p>
+        <div className='buttons grid-area'>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+        </div>
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
